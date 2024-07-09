df4b4253fc48f4b4809f880c9ca25a8777d6057d
anjali29singh <anjalisingh930260@gmail.com>
2024-07-09 11:45:50 +0530

    changed data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,8 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
-}
+  email: "anjalisighdev2@gmail.com",
+  phone: "+919302609010",
+  address: "Madhya Pradesh Bhopal ",
+  github: "https://github.com/anjali29singh",
+  linkedIn: "https://www.linkedin.com/in/anjali-singh-0b3519202/",
+  twitter: "https://x.com/anjali29singh",
+};
--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,51 +1,77 @@
-import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import ayla from "/public/image/ayla.jpg";
+import crefin from "/public/image/crefin.jpg";
+import realEstate from "/public/image/real-estate.jpg";
+import travel from "/public/image/travel.jpg";
 
 export const projectsData = [
-    {
-        id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
-        code: '',
-        demo: '',
-        image: crefin,
-    },
-    {
-        id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
-        code: '',
-        demo: '',
-        image: travel,
-    },
-    {
-        id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-    },
-    {
-        id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
-    }
+  {
+    id: 1,
+    name: "Tech News App ",
+    description:
+      "I have developed an android app called Tech News App. The app fetches the latest tech news from the News API and displays them in a recycler view. I used Retrofit to fetch the data from the API and Glide to load the images. I implemented the MVVM architecture pattern and used LiveData and ViewModel to handle the data",
+    tools: [
+      "Java",
+      "RESTAPI",
+      "Retrofit",
+      "AWS SES",
+      "Glide",
+      "LiveData",
+      "Android Studio",
+    ],
+    role: "Android developer",
+    code: "",
+    demo: "",
+    image: crefin,
+  },
+  {
+    id: 2,
+    name: "Automatic Irrigation System",
+    description:
+      "I developed an group project known as Automatic Irrigation System that uses a soil moisture sensor to detect the moisture level and turn on the water pump when the moisture level is below a certain threshold. When it crosses the threshold is automatically stops the water pump.",
+    tools: [
+      "Keil micro vision",
+      "C++",
+      "8051 microcontroller",
+      "Soil Moisture Sensor",
+      "Motor Pump etc",
+    ],
+    role: "",
+    code: "",
+    demo: "",
+    image: travel,
+  },
+  {
+    id: 3,
+    name: "Shrink Url",
+    description:
+      " A fullstack application that allows users to shorten long URLS. We can share and use this url everytime.",
+    tools: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Express",
+      ,
+      "MongoDB",
+      "Mircosoft Azure",
+      "Docker",
+    ],
+    code: "",
+    role: "Full Stack Developer",
+    demo: "",
+    image: realEstate,
+  },
+  {
+    id: 4,
+    name: "Code conversion tool (codetransit)",
+    description:
+      "A web application that allows users to convert code from one programming language to another. It supports multiple programming languages and provides a user-friendly interface for code conversion.",
+    tools: ["ReactJS", "TailwindCss", "OpenAI API", "Material UI", "Vscode"],
+    code: "",
+    demo: "",
+    image: ayla,
+    role: "Frontend Developer",
+  },
 ];
-
 
 // Do not remove any property.
 // Leave it blank instead as shown below
